170f2baebf271d7b22c5bea41ee2a08d2887cd07
lengkhang <lengkhang@gmail.com>
2021-03-28 20:04:17 +0800

    Add scrollview in dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, ScrollView } from 'react-native';
 import { Title, Caption } from 'react-native-paper';
 
 import { useNavigation } from '@react-navigation/native';
@@ -32,43 +32,49 @@ export default function Dashboard() {
   }, [subjects.sampleUser.first_name]);
 
   // console.log('==> sampleUser:', sampleUser);
-  console.log('==> Dashboard-expenses:', totalExpensesThisMonth, groupByDate);
-  const sampleData = {"03 January 2021": [{"amount": "5", "category": "GROCERY", "date": '2021-01-03T08:05:43.000Z', "description": "Highly"}], "20 February 2021": [{"amount": "4", "category": "CAR", "date": '2021-02-20T08:05:24.000Z', "description": "Ugh"}], "28 March 2021": [{"amount": "1", "category": "FOOD_AND_DRINK", "date": '2021-03-28T08:04:58.688Z', "description": "Dfsd"}, {"amount": "2", "category": "GIFT", "date": '2021-03-28T08:05:06.591Z', "description": "Diff"}, {"amount": "3", "category": "HOUSE", "date": '2021-03-28T08:05:14.299Z', "description": "Thug"}]};
+  console.log('==> Dashboard-expenses:', totalExpensesThisMonth, groupByDate, sortedExpenses);
+  const sampleData = {};// = {"1614441600000": [{"amount": 2000, "category": "CAR", "date": '2021-02-28T11:47:41.000Z', "description": "Car"}], "1615046400000": [{"amount": 0.5, "category": "FOOD_AND_DRINK", "date": '2021-03-07T11:48:49.000Z', "description": "Milo"}], "1616774400000": [{"amount": 100, "category": "GIFT", "date": '2021-03-27T11:50:09.000Z', "description": ""}], "1616860800000": [{"amount": 99999999, "category": "GIFT", "date": '2021-03-28T11:51:19.159Z', "description": "Test"}, {"amount": 200, "category": "FOOD_AND_DRINK", "date": '2021-03-28T11:49:56.509Z', "description": ""}, {"amount": 500, "category": "HOUSE", "date": '2021-03-28T11:47:21.762Z', "description": "Rent"}, {"amount": 100, "category": "GIFT", "date": '2021-03-28T11:45:51.274Z', "description": "Cp owe"}]};
+  const sampleSortKey = null;// = ["1616860800000", "1616774400000", "1615046400000", "1614441600000"];
 
   return (
-    <View style={styles.container}>
-      <Text>Welcome {sampleUser?.first_name},</Text>
-      <MonthlyExpenseWidget caption="Total expenses this month" amount={`$ ${totalExpensesThisMonth}`} />
-      {
-        sortedExpenses.map((date, i) => {
-          return (<View key={`${date}-${i}`}>
-            <Caption>{moment(new Date(parseInt(date))).format(dateFormat)}</Caption>
-            {
-              groupByDate[date].map((expensesForTheDay, j) => {
-                const { description, category, amount } = expensesForTheDay;
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Title>Welcome {sampleUser?.first_name},</Title>
 
-                return (
-                  <View key={`${expensesForTheDay}-${j}`} style={styles.card}>
-                    <ExpenseCard
-                      category={category}
-                      description={description}
-                      amount={amount}
-                    />
-                  </View>
-                )
-              })
-            }
-            
-          </View>);
-        })
-      }
-    </View>
+        <MonthlyExpenseWidget caption="Total expenses this month" amount={`$ ${totalExpensesThisMonth}`} />
+        {
+          (sampleSortKey || sortedExpenses).map((date, i) => {
+            return (<View key={`${date}-${i}`}>
+              <Caption>{moment(new Date(parseInt(date))).format(dateFormat)}</Caption>
+              
+              {
+                (sampleData[date] || groupByDate[date]).map((expensesForTheDay, j) => {
+                  const { description, category, amount } = expensesForTheDay;
+
+                  return (
+                    <View key={`${expensesForTheDay}-${j}`} style={styles.card}>
+                      <ExpenseCard
+                        category={category}
+                        description={description}
+                        amount={amount}
+                      />
+                    </View>
+                  )
+                })
+              }
+            </View>);
+          })
+        }
+       </ScrollView>
+     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
+  },
+  scrollView: {
     paddingTop: 20,
     paddingHorizontal: 15,
     backgroundColor: '#fff'
